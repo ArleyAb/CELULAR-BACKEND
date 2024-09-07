ba104e4b2d0bda2d114d4c3494d7b95b17abe785
Astrid <asarabhe@hotmail.com>
2024-09-07 02:58:56 -0600

    Initial commit

---
+++ src/main/resources/import.sql
@@ -0,0 +1,6 @@
+insert into celulares (name,marca,created_at) values ('Samsung','S21', now());
+insert into celulares (name,marca,created_at) values ('Samsung','S22', now());
+insert into celulares (name,marca,created_at) values ('Samsung','S23', now());
+insert into celulares (name,marca,created_at) values ('Apple','iphone 14', now());
+insert into celulares (name,marca,created_at) values ('Apple','iphone 15', now());
+insert into celulares (name,marca,created_at) values ('Xiaomi','Mi', now());
---
+++ src/test/java/com/example/SpringbootServiceCelularesApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.microservice.celulares.SpringbootServiceCelularesApplication;
+
+@SpringBootTest(classes = SpringbootServiceCelularesApplication.class)
+class SpringbootServiceCelularesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
